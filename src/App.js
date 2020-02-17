47f4a76da645d870b0fd7275b7a1071b8c7e77de
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-02-17 12:40:57 -0500

    Setting up basic routing: history param location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
+
 
 import HomePage from './pages/home-page'
 
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
 function App() {
   return (
-    <div className="App">
-      <HomePage/>>
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory-menu/directory-menu.js
+++ src/components/directory-menu/directory-menu.js
@@ -48,8 +48,14 @@ class DirectoryMenu extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+          <MenuItem
+            key={id}
+            title={title}
+            imageUrl={imageUrl}
+            size={size}
+            linkUrl={linkUrl}
+          />
         ))}
       </div>
     );
