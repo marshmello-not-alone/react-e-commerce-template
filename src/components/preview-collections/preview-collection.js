c7430150a47588405fcc9858a55bc574f1577adc
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-02-17 17:27:44 -0500

    Adding the menu items to the collection preview page

--- src/components/preview-collections/preview-collection.js
+++ src/components/preview-collections/preview-collection.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./preview-collection.scss";
 
+import CollectionItem from '../collections-item/collections-item'
+
 const CollectionPreview = ({ title, items }) => (
   <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
     <div className="preview">
       {items
         .filter((item, idx) => idx < 4)
-        .map(item => (
-          <div key={item.id}>{item.name}</div>
+        .map(({id, ...otherProps}) => (
+          <CollectionItem key={id} {...otherProps}/>
         ))}
     </div>
   </div>
